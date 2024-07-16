9207dfe67756fcae29f8e13ec827ca22f68cc558
Fabian Jhon Muñoz Puello <78973548+fabianmunozp2508@users.noreply.github.com>
2024-07-16 11:45:46 -0500

    tenanid

--- src/middleware/tenantMiddleware.js
+++ src/middleware/tenantMiddleware.js
@@ -1,42 +1,35 @@
-// src/middleware/tenantMiddleware.js
+// tenantMiddleware.js
 const { Pool } = require('pg');
-const config = require('config');
+const pool = new Pool({ connectionString: process.env.POSTGRES_URI });
 
-const pool = new Pool({ connectionString: config.get('postgresURI') });
+async function getTenantIdFromSubdomain(subdomain) {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT id FROM tenants WHERE subdomain = $1', [subdomain]);
+    client.release();
+    
+    if (result.rows.length === 0) {
+      return null; // Subdominio no encontrado
+    }
+    
+    return result.rows[0].id;
+  } catch (err) {
+    console.error('Error fetching tenant ID:', err);
+    return null;
+  }
+}
 
-const tenantMiddleware = async (req, res, next) => {
+module.exports = async (req, res, next) => {
   const subdomain = req.subdomains[0];
   if (!subdomain) {
     return res.status(400).send('No tenant specified');
   }
 
-  try {
-    const tenantId = await getTenantIdFromSubdomain(subdomain);
-    if (!tenantId) {
-      return res.status(400).send('Invalid tenant specified');
-    }
-
-    // Asigna el tenant_id basado en el subdominio
-    req.tenant_id = tenantId;
-    next();
-  } catch (error) {
-    console.error('Error fetching tenant ID:', error);
-    res.status(500).send('Server error');
+  const tenantId = await getTenantIdFromSubdomain(subdomain);
+  if (!tenantId) {
+    return res.status(404).send('Tenant not found');
   }
-};
 
-const getTenantIdFromSubdomain = async (subdomain) => {
-  try {
-    const result = await pool.query('SELECT id FROM tenants WHERE subdomain = $1', [subdomain]);
-    if (result.rows.length > 0) {
-      return result.rows[0].id;
-    } else {
-      return null;
-    }
-  } catch (error) {
-    console.error('Error querying tenant ID from subdomain:', error);
-    throw error;
-  }
+  req.tenant_id = tenantId;
+  next();
 };
-
-module.exports = tenantMiddleware;
